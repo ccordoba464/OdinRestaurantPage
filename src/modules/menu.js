cd8b3d120ebb94b2f073acba0297144fd4d68dbb
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-07-03 17:36:22 -0400

    Add menu items to menu container

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -6,6 +6,26 @@ function loadMenu() {
   const menu = document.createElement("div");
   menu.id = "menu";
 
+  const menuItem1 = document.createElement("div");
+  menuItem1.classList.add("menu-item");
+  menuItem1.textContent = "Cheese Pizza: Gold AR level Pizza";
+
+  const menuItem2 = document.createElement("div");
+  menuItem2.classList.add("menu-item");
+  menuItem2.textContent =
+    "Pepperoni Pizza: SJ and Zeke sliced pepperoni slices on cheese";
+
+  const menuItem3 = document.createElement("div");
+  menuItem3.classList.add("menu-item");
+  menuItem3.textContent = "Meat Lovers Pizza: If you love meat :D";
+
+  const menuItem4 = document.createElement("div");
+  menuItem4.classList.add("menu-item");
+  menuItem4.textContent =
+    "Hawaiian Pizza: Cheese Pizza but made by a Hawaiian dude";
+
+  menu.append(menuItem1, menuItem2, menuItem3, menuItem4);
+
   home.appendChild(menu);
 }
 
