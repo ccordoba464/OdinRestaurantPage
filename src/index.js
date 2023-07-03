8426f8185c69a4eb8e7a4ea707e8c19dc9fa1bb9
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-07-03 17:01:46 -0400

    Add event listeners to tab buttons and import all module functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import loadPage from "./modules/initial-page-load";
 import loadHome from "./modules/home";
+import loadContact from "./modules/contact";
+import loadMenu from "./modules/menu";
 
 init();
+
+function addNavEvents() {
+  const homeTab = document.getElementById("homeTab");
+  const contactTab = document.getElementById("contactTab");
+  const menuTab = document.getElementById("menuTab");
+
+  homeTab.addEventListener("click", loadHome);
+  contactTab.addEventListener("click", loadContact);
+  menuTab.addEventListener("click", loadMenu);
+}
 
 function init() {
   loadPage();
   loadHome();
+  addNavEvents();
 }
