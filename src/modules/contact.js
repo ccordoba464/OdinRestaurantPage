d099dca861ee0ad009bc49b3628ad7d3cfe90ba3
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-07-03 17:21:53 -0400

    Create contacts and add to contacts container

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -6,6 +6,20 @@ function loadContact() {
   const contacts = document.createElement("div");
   contacts.id = "contacts";
 
+  const contact1 = document.createElement("div");
+  contact1.classList.add("contact");
+  contact1.textContent = "Chef: Rimbo";
+
+  const contact2 = document.createElement("div");
+  contact2.classList.add("contact");
+  contact2.textContent = "Manager: Zeke";
+
+  const contact3 = document.createElement("div");
+  contact3.classList.add("contact");
+  contact3.textContent = "Waiter: SJ";
+
+  contacts.append(contact1, contact2, contact3);
+
   home.appendChild(contacts);
 }
 
